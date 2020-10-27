9017b8c687ca3d4fedf80c6c4bb26f74bde90b43
keshiamukenge <keshia_mkg@live.fr>
2020-10-27 21:09:34 +0100

    Ajout du défi 3 et début de la mise en forme

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-function somme(){
+// Défi 1 :
+// Faire l'addition de 2 nombres défini par l'utilisateur
+function defi1(){
     var nbr1 = Number(document.getElementById("nbr1").value);
     var nbr2 = Number(document.getElementById("nbr2").value);
     var somme = nbr1 + nbr2;
     document.getElementById("somme").value = somme;
 }
 
-function multiple(){
+// Défi 2 :
+// Lister tous les multiples d'un nombre défini par l'utilisateur
+function defi2(){
     var list =[];
     var nbr3 = Number(document.getElementById("nbr3").value);
     for( let i = 0; i <= nbr3; i++ ){
@@ -13,10 +17,41 @@ function multiple(){
            list.push(i);
         }
     }
-    document.getElementById("multiple").value = list;    
+    document.getElementById("multiple").value = list;  
 }
 
-function diviseur(){
-    var nbr4 = Number(document.getElementById("nbr4").value);
-    
+// Défi 3 :
+// Définir si le nombre entrer par l'utilisateur est un nombre premier ou pas
+function diviseur(nbr4){
+    var list =[];
+    for( let i = 0; i <= nbr4; i++ ){
+        if (nbr4 % i == 0 ){
+           list.push(i);
+        }
+    }
+    return list;   
 }
+
+function defi3(){
+    var nbr4 = Number(document.getElementById("nbr4").value);
+    var listeSolutions = diviseur(nbr4);
+    if (listeSolutions.length == 2) {
+        alert("C'est un nombre premier");
+    } else {
+        alert("Ce n'est pas un nombre premier");
+    }
+}
+
+// Défi 6 :
+// Définir si le mot entré par l'utilisateur est un palindrome ou pas
+function defi6(){
+    var word = document.getElementById("motUtilisateur").value;
+    word = normalizeString(word);
+    let reverse = word.reverse();
+    if (word == reverse ){
+        alert("Le mot est un palindrome");
+    } else{
+        alert("Le mot n'est pas un palindrome");
+    }
+}
+    
