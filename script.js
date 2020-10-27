b9684bb9262ca07faddb8b5d6855e72b0aa7a6d5
keshiamukenge <keshia_mkg@live.fr>
2020-10-27 23:16:23 +0100

    Changement des couleurs et ajout d'icônes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Défi 1 :
 // Faire l'addition de 2 nombres défini par l'utilisateur
-function defi1(){
+function defi1() {
     var nbr1 = Number(document.getElementById("nbr1").value);
     var nbr2 = Number(document.getElementById("nbr2").value);
     var somme = nbr1 + nbr2;
@@ -9,30 +9,30 @@ function defi1(){
 
 // Défi 2 :
 // Lister tous les multiples d'un nombre défini par l'utilisateur
-function defi2(){
-    var list =[];
+function defi2() {
+    var list = [];
     var nbr3 = Number(document.getElementById("nbr3").value);
-    for( let i = 0; i <= nbr3; i++ ){
-        if (nbr3 % i == 0 ){
-           list.push(i);
+    for (let i = 0; i <= nbr3; i++) {
+        if (nbr3 % i == 0) {
+            list.push(i);
         }
     }
-    document.getElementById("multiple").value = list;  
+    document.getElementById("multiple").value = list;
 }
 
 // Défi 3 :
 // Définir si le nombre entrer par l'utilisateur est un nombre premier ou pas
-function diviseur(nbr4){
-    var list =[];
-    for( let i = 0; i <= nbr4; i++ ){
-        if (nbr4 % i == 0 ){
-           list.push(i);
+function diviseur(nbr4) {
+    var list = [];
+    for (let i = 0; i <= nbr4; i++) {
+        if (nbr4 % i == 0) {
+            list.push(i);
         }
     }
-    return list;   
+    return list;
 }
 
-function defi3(){
+function defi3() {
     var nbr4 = Number(document.getElementById("nbr4").value);
     var listeSolutions = diviseur(nbr4);
     if (listeSolutions.length == 2) {
@@ -44,14 +44,13 @@ function defi3(){
 
 // Défi 6 :
 // Définir si le mot entré par l'utilisateur est un palindrome ou pas
-function defi6(){
+function defi6() {
     var word = document.getElementById("motUtilisateur").value;
     word = normalizeString(word);
     let reverse = word.reverse();
-    if (word == reverse ){
+    if (word == reverse) {
         alert("Le mot est un palindrome");
-    } else{
+    } else {
         alert("Le mot n'est pas un palindrome");
     }
 }
-    
